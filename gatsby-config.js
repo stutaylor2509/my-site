4f04ea520443dc0d4d362ff5a70c443ca83aac3d
stutaylor2509 <57462368+stutaylor2509@users.noreply.github.com>
2021-12-10 15:09:53 +0000

    First commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `sdott81`,
+    description: `My Gatsby based portfolio`,
+    author: `Stu Taylor`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
@@ -31,8 +31,23 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    
+  
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+  {
+    resolve: 'gatsby-plugin-google-fonts',
+    options: {
+      fonts: [
+        `Lato`,
+        `IBM+Plex+Sans\:400`
+      ],
+      display: "swap",
+    },
+  },
   ],
 }
+
+
+     
--- src/components/layout.js
+++ src/components/layout.js
@@ -9,7 +9,6 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
@@ -25,12 +24,9 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
       <div
         style={{
           margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
         }}
       >
         <main>{children}</main>
