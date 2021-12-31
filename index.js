7cdfe38bf5bf97204610b047ff91b8dd67c3e70e
stutaylor2509 <57462368+stutaylor2509@users.noreply.github.com>
2021-12-31 13:41:55 +0000

    Update again

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
-import HomeVideo from "../images/splash.mp4"
+//import { Link } from "gatsby"
+//import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
@@ -11,19 +10,12 @@ const IndexPage = () => (
     
     <Seo title="Home" />
    
-    <section>
-     <div class="clip--container">
-    <video autoPlay muted loop>
-      <source src={HomeVideo} type="video/mp4" />
-    </video>
-    <svg viewBox="0 0 100 100">
-        <defs>
-            <clipPath clipPathUnits="objectBoundingBox" id="mask">
-                <path class="cls-1" d="M0,.64H.24A.16.16,0,0,0,.43.81C.55.81.59.75.59.71.59.51,0,.67,0,.29,0,.15.11,0,.41,0s.4.15.4.34H.57C.57.26.54.19.4.19S.26.24.26.28.82.31.82.69C.82.83.75,1,.41,1S0,.86,0,.64Z"/>
-            </clipPath>
-        </defs>
-        </svg>
+    <section className="hero">
+     <div className="clip--container">
+  <h1>sdott81</h1>
+  <h3>web development | custom wordpress themes | digital marketing</h3>
         </div> 
+        
     </section>
   </Layout>
 )
